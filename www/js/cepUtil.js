c5427f8db03817bbaf57f2048e46be0b299570c1
Claudenir Freitas <claudenirfreitas.cf@gmail.com>
2017-10-11 09:01:11 -0300

    sugestão
    Cara, bati o olho na função que valida o CEP e vi uma melhoria, vê aí se faz sentido.

--- www/js/cepUtil.js
+++ www/js/cepUtil.js
@@ -3,10 +3,7 @@ var cepUtil = (function () {
 
     function validar(cep) {
         var objER = /^[0-9]{2}.[0-9]{3}-[0-9]{3}$/;
-        if (!cep || !objER.test(cep)) {
-            return false;
-        }
-        return true;
+        return cep && objER.test(cep);
     }
 
     var colocarMascara = function (cepSemFormatacao) {
